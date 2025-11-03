590c7e1cc575569d821582c5c77f2dc8e363e85c
Davidoll <46897207+Davidoll@users.noreply.github.com>
2025-11-03 20:56:35 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,14 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Gener kernel for Xiaomi 6 sagit
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
+device.name1=sagit
+supported.versions=13-16
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
